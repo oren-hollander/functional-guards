e2111b21338d1a0ce18958c7506fbedf54a91cbc
orenh <orenh@wix.com>
2016-03-29 14:47:03 +0300

    Document Services API demo

--- src/api.js
+++ src/api.js
@@ -36,21 +36,19 @@ const DocumentServices = () => {
     return x
   }
 
-  const readApi = {
-    get: key => getValue(key)(map)
-  }
-
   return {
     create: value => {
       const key = nextKey()
       queue = [...queue, {op: 'set', key, value}]
-      return key
+      return Promise.resolve(key)
     },
-    set: (key, value) => {
+    set: key => value => {
       queue = [...queue, {op: 'set', key, value}]
+      return Promise.resolve()
     },
     del: key => {
       queue = [...queue, {op: 'del', key}]
+      return Promise.resolve()
     },
     get: key => {
       if(drainPromise){
@@ -63,54 +61,25 @@ const DocumentServices = () => {
       else {
         return Promise.resolve(map[key])
       }
-    },
-    read: f => {
-      if(drainPromise){
-        drainPromise.then(() => f(readApi))
-      }
-      else if(queue.length > 0){
-        drainPromise = drain()
-        return drainPromise.then(resetDrainPromise).then(() => f(readApi))
-      }
-      else {
-        f(readApi)
-      }
     }
   }
 }
 
-const print = x => console.log(x)
+const print = x => {
+  console.log(x)
+  return x
+}
 
 const ds = DocumentServices()
 
-console.log('create 3 entries')
-const p1 = ds.create('value 1')
-const p2 = ds.create('value 2')
-const p3 = ds.create('value 3')
+const mul = x => y => x * y
+const getX = _ => ds.get('x')
+const setX = ds.set('x')
 
-console.log('read 2 entries using promises - expect one queue drain for both reads')
-ds.get(p1).then(print)
-ds.get(p2).then(print)
-
-setTimeout(() => {
-  console.log('set and then read an entry - expect one queue drain')
-  ds.set(p1, 'value 4')
-  ds.get(p1).then(print)
-}, 2000)
-
-setTimeout(() => {
-  console.log('set and then read two entries - expect one queue drain for both reads')
-  ds.set(p1, 'value 5')
-  ds.set(p2, 'value 6')
-  ds.read((api) => {
-    console.log(api.get(p1))
-    console.log(api.get(p2))
-  })
-}, 4000)
-
-setTimeout(() => {
-  console.log('set one entry and then read another entry - expect no queue drain')
-  ds.set(p3, 'value 7')
-  ds.get(p1).then(print)
-}, 6000)
-
+setX(3)
+  .then(getX)
+  .then(print)
+  .then(mul(2))
+  .then(setX)
+  .then(getX)
+  .then(print)
